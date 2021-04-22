6e76747e4907a06a6b1bfc704c4418f9f21dd7c9
Lucas Meier <meiluca@dunwoody.edu>
2021-04-21 22:48:46 -0500

    add routing api

---
+++ web_app/routes/api.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+    //url from api
+    const url = 'https://www.petrescue.com.au/api/listings/:id';
+    
+    const request = https.request(url,(response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data = data + chunk.toString();
+        });
+
+        response.on('end', () => {
+            const body = JSON.parse(data);
+            console.log(body);
+            res.render('apidata', {body});
+        });
+    });
+
+    request.on('error', (error) => {
+        console.log('An error', error);
+    });
+
+    request.end()
+
+
+});
+  gt  
+module.exports = router;
